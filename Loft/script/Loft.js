dda2599d46e01957cd34b2460ba6db8d647389da
Cashpure <152092379+Cashpure@users.noreply.github.com>
2024-01-01 01:45:20 +0300

    Update Loft.js

--- Loft/script/Loft.js
+++ Loft/script/Loft.js
@@ -80,6 +80,24 @@ $(function () {
          $('.cta__info-text').delay(600).addClass('cta-text__active')
       }
    }, 600)
+
+
+      $(".header__nav-link").on("click", function (e) {
+      var anchor = $(this);
+      $('html, body').stop().animate({
+         scrollTop: $(anchor.attr('href')).offset().top
+      }, 777);
+      e.preventDefault();
+      return false;
+   });
+   $(".footer-nav__link").on("click", function (e) {
+      var anchor = $(this);
+      $('html, body').stop().animate({
+         scrollTop: $(anchor.attr('href')).offset().top
+      }, 777);
+      e.preventDefault();
+      return false;
+   });
 });
 
 
