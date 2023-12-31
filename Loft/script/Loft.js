8057282c232148a8f689c71b7a3a341582533c5f
Cashpure <152092379+Cashpure@users.noreply.github.com>
2023-12-31 22:36:31 +0300

    Update Loft.js

--- Loft/script/Loft.js
+++ Loft/script/Loft.js
@@ -87,14 +87,7 @@ $('.header__nav-link').on('click', function () {
    $('.header__nav').toggleClass('header__nav--open');
    $('.burger').toggleClass('burger--active')
 });
-   $("a[href*='#']").on("click", function (e) {
-      var anchor = $(this);
-      $('html, body').stop().animate({
-         scrollTop: $(anchor.attr('href')).offset().top
-      }, 777);
-      e.preventDefault();
-      return false;
-   });
+
 
 
 // initMap();
